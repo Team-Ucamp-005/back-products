c3e2a7780e65962b386757a06a11b225bfc2047d
Alfredo Hernandez Noriega <alfredo.hernandez@kushkipagos.com>
2022-06-25 13:12:39 -0500

    create product

--- apis/products.js
+++ apis/products.js
@@ -1,14 +1,38 @@
 const express = require('express');
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 const router = express.Router();
 
 const { productsController } = require('../controllers');
-const { getProducts } = productsController
+const { getProducts, createProduct } = productsController
 
 
 router.get('/', async (req, res) => {
   const products = await getProducts()
   res.send(products)
+})
+
+router.post('/', async (req, res) => {
+  const body = req.body
+  console.log(body)
+
+  try {
+    const newProduct = await createProduct(body)
+    res.status(200)
+    res.send(newProduct)
+  } catch (error) {
+    console.log(error)
+    if (error instanceof mongoose.Error.ValidationError) {
+      res.status(400)
+      res.send({
+        message: 'Error de validacion',
+        reason: error.message
+      })
+      res.status(500)
+      return res.send({
+        error: error.message
+      })
+    }
+  }
 })
 
 
--- controllers/products.js
+++ controllers/products.js
@@ -5,8 +5,15 @@ const getProducts = async () => {
   return ProductModel.find({})
 }
 
+const createProduct = async (body) => {
+  const newProduct = new ProductModel(body)
+  await newProduct.save()
+  return newProduct
+}
+
 
 
 module.exports = {
-  getProducts
+  getProducts,
+  createProduct
 }
